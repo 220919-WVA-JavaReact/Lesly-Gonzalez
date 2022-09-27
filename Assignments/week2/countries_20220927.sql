c634b246b707e20a3f6d17b15d70f47ae05ff15f
Lesly <leslyr562@gmail.com>
2022-09-27 13:59:12 -0500

    sql countries

---
+++ Assignments/week2/countries_20220927.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.countries ("countryName",continent,population,"nearOcean","funFact") VALUES
+	 ('Madagascar','Africa',27000000,true,'two seasons are recognized: a hot, rainy and a cooler, dry.'),
+	 ('Spain','Europe',47000000,true,'fourteen typical dishes'),
+	 ('Philippines','Asia',109000000,true,'consists of around 7,641 islands'),
+	 ('Argentina','South America',45000000,true,'the second-largest country in South America'),
+	 ('Paraguay','South America',7000000,false,'home to large swaths of swampland, subtropical forest and chaco');
